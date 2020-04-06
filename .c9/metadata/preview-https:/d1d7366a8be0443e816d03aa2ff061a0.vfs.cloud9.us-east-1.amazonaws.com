7144a2997fc6275a78d2d292cba440797122be1c
EC2 Default User <ec2-user@ip-172-31-87-211.ec2.internal>
2020-04-06 00:46:34 +0000

    added hw1

--- .c9/metadata/preview-https:/d1d7366a8be0443e816d03aa2ff061a0.vfs.cloud9.us-east-1.amazonaws.com
+++
@@ -1,1 +0,0 @@
-{"changed":false,"filter":false,"title":"[P] https://d1d7366a8be0443e816d03aa2ff061a0.vfs.cloud9.us-east-1.amazonaws.com","value":"","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://d1d7366a8be0443e816d03aa2ff061a0.vfs.cloud9.us-east-1.amazonaws.com","previewer":"previewBrowser","disableInjection":true}}
--- app.js
+++ app.js
@@ -13,18 +13,17 @@ app.get('/', function(req, res){
 });
 
 /* The handler for the /results route */
-app.get('/results', function(req, res){
-	var query = req.query.search;
-	var url = 'https://openlibrary.org/api/books?bibkeys=ISBN:' + query +'&format=json&jscmd=data';
-	request(url, function(error, response, dataStream){
-		if (!error && response.statusCode == 200){
-			var data = JSON.parse(dataStream);
-			console.log('data=',data);
-			res.render('results', {data: data});
-		}
-	});
+app.get('/intro', function(req, res){
+	res.render('intro')
 });
 
+app.get('/demo', function(req, res){
+	res.render('demo')
+});
+
+app.get('/types', function(req, res){
+	res.render('types')
+});
 /* The handler for undefined routes */
 app.get('*', function(req, res){
    res.render('error'); 
