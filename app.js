c004bfa00b38ecdc1d843f877c5b1bd4c3e2c3a8
EC2 Default User <ec2-user@ip-172-31-87-211.ec2.internal>
2020-04-20 07:14:51 +0000

    database lab 9

--- app.js
+++ app.js
@@ -1,29 +1,115 @@
 /* Require external APIs and start our application instance */
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
-var request = require('request');
 
 /* Configure our server to read public folder and ejs files */
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+/* Configure MySQL DBMS */
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'jose',
+    password: 'jose',
+    database: 'quotes_db'
+});
+connection.connect();
 
 /* The handler for the DEFAULT route */
 app.get('/', function(req, res){
     res.render('home');
 });
 
-/* The handler for the /results route */
-app.get('/intro', function(req, res){
-	res.render('intro')
+app.get('/names', function(req, res){
+    res.render('names');
+
 });
 
-app.get('/demo', function(req, res){
-	res.render('demo')
+app.get('/gender', function(req, res){
+    res.render('gender');
+
 });
 
-app.get('/types', function(req, res){
-	res.render('types')
+app.get('/category', function(req, res){
+    res.render('category');
+
 });
+
+app.get('/genderSearch', function(req, res){
+    var stmt = 'select * from l9_author where sex=\''
+                +req.query.gender + '\';'
+    connection.query(stmt, function(error, found){
+	    var author = null;
+	    if(error) throw error;
+	    if(found.length){
+	        author = found[0];
+	        // Convert the Date type into the String type
+	        author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+	        author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+	        
+	        author = found[1];
+	        // Convert the Date type into the String type
+	        author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+	        author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+	       
+	        author = found[2];
+	        // Convert the Date type into the String type
+	        author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+	        author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+	        
+	          author = found[3];
+	        // Convert the Date type into the String type
+	        author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+	        author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+	    }
+	    res.render('genderSearch', {author: found});
+	});
+        
+});
+
+app.get('/quotesSearch', function(req, res){
+    var stmt = 'select * from l9_quotes where category=\''
+                +req.query.category + '\';'
+    connection.query(stmt, function(error, results){
+        if(error) throw error;
+        var name = results[0].category;
+        res.render('quotes2', {name: name, quotes: results});      
+    });
+
+        
+});
+/* The handler for the /author route */
+app.get('/author', function(req, res){
+    var stmt = 'select * from l9_author where firstName=\'' 
+                + req.query.firstname + '\' and lastName=\'' 
+                + req.query.lastname + '\';'
+	connection.query(stmt, function(error, found){
+	    var author = null;
+	    if(error) throw error;
+	    if(found.length){
+	        author = found[0];
+	        // Convert the Date type into the String type
+	        author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+	        author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+	    }
+	    res.render('author', {author: author});
+	});
+});
+
+/* The handler for the /author/name/id route */
+app.get('/author/:aid', function(req, res){
+    var stmt = 'select quote, firstName, lastName ' +
+               'from l9_quotes, l9_author ' +
+               'where l9_quotes.authorId=l9_author.authorId ' + 
+               'and l9_quotes.authorId=' + req.params.aid + ';'
+    connection.query(stmt, function(error, results){
+        if(error) throw error;
+        var name = results[0].firstName + ' ' + results[0].lastName;
+        res.render('quotes', {name: name, quotes: results});      
+    });
+});
+
 /* The handler for undefined routes */
 app.get('*', function(req, res){
    res.render('error'); 
